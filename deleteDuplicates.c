b0eac04de74ec763ba4f6b1aa511bfcabe813154
SuperBypassUdinnn <maingametiaplibur86@gmail.com>
2025-05-08 22:30:31 +0700

    menghapus test case dan fungsi-fungsi terkait

--- deleteDuplicates.c
+++ deleteDuplicates.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <stdlib.h>
 
 // Definisi struktur node
@@ -22,67 +21,4 @@ struct ListNode* deleteDuplicates(struct ListNode* head) {
     }
 
     return head;
-}
-
-// Fungsi untuk membuat node baru
-struct ListNode* createNode(int val) {
-    struct ListNode* newNode = (struct ListNode*)malloc(sizeof(struct ListNode));
-    newNode->val = val;
-    newNode->next = NULL;
-    return newNode;
-}
-
-// Fungsi untuk mencetak linked list
-void printList(struct ListNode* head) {
-    struct ListNode* temp = head;
-    while (temp != NULL) {
-        printf("%d ", temp->val);
-        temp = temp->next;
-    }
-    printf("\n");
-}
-
-// Fungsi untuk membuat linked list dari input pengguna
-struct ListNode* createListFromInput() {
-    int n, val;
-    struct ListNode *head = NULL, *tail = NULL;
-
-    printf("Masukkan jumlah elemen dalam linked list: ");
-    scanf("%d", &n);
-
-    if (n <= 0) {
-        return NULL;
-    }
-
-    printf("Masukkan elemen-elemen (urutkan dalam ascending):\n");
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &val);
-        struct ListNode* newNode = createNode(val);
-
-        if (head == NULL) {
-            head = newNode;
-            tail = newNode;
-        } else {
-            tail->next = newNode;
-            tail = newNode;
-        }
-    }
-
-    return head;
-}
-
-// Driver code
-int main() {
-    printf("Buat linked list:\n");
-    struct ListNode* head = createListFromInput();
-
-    printf("Original list: ");
-    printList(head);
-
-    head = deleteDuplicates(head);
-
-    printf("List setelah menghapus duplikat: ");
-    printList(head);
-
-    return 0;
 }
