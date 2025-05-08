d6558009d2d1847f25fcd2c8e081e2e521dfa0f5
IrfanQadafi <qadafi.irfan.m@gmail.com>
2025-05-08 18:31:36 +0700

    soal7.c

---
+++ soal7.c
@@ -0,0 +1,22 @@
+struct ListNode* removeElements(struct ListNode* head, int val) {
+    // Membuat dummy node untuk menangani kasus saat head perlu dihapus
+    struct ListNode dummy;
+    dummy.next = head;
+
+    // Pointer untuk traversal
+    struct ListNode* current = &dummy;
+
+    // Lakukan traversal pada linked list
+    while (current->next != NULL) {
+        if (current->next->val == val) {
+            // Jika node berikutnya harus dihapus, lewati node itu
+            struct ListNode* temp = current->next;
+            current->next = current->next->next;
+            free(temp);
+        } else {
+            current = current->next;
+        }
+    }
+
+    return dummy.next;
+}
