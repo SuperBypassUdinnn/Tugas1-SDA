1885cc827b2bb8137f500e2bb52f0a4640aa4659
syifa athiyya <syifaathiyyaa@gmail.com>
2025-05-09 09:24:03 +0700

    Create isPalindrome.c

---
+++ isPalindrome.c
@@ -0,0 +1,42 @@
+include<stdio.h>
+include<stdbool.h>
+
+bool isPalindrome(struct ListNode* head){
+    
+    int stack [105]; //105 sesuai syarat di soal
+    int count = 0;   //nyimpan panjang node
+    int end = 0;     //perbandingan node dan stack;
+    
+    struct ListNode* temp = head;
+
+  // ngitung panjang node
+    while (temp != NULL) {
+        count++;
+        temp = temp->next;
+        }
+ // nyimpan isi node secara terbalik di array stack
+        temp = head;
+    for (int i = count-1; i >= 0; i--){
+        stack[i] =  temp->val;
+        temp = temp->next;
+    }
+
+// ngecek apakah isi node dan array stack sama
+        temp = head;
+    for (int i = 0; i < count; i++){
+        if(stack[i] == temp->val){
+            end++;
+            temp = temp->next;
+        }
+        else{
+            continue;
+        }
+    }
+        
+      if (end == count){
+        return true;
+      }
+      else{
+        return false;
+      }
+    }
