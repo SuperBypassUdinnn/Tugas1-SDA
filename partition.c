c3a2371be17fa8242125f028c34d238e567149ae
SuperBypassUdinnn <maingametiaplibur86@gmail.com>
2025-05-12 15:46:57 +0700

    style: mengubah nama variabel dan mengubah comment

--- partition.c
+++ partition.c
@@ -1,9 +1,10 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 // Definisi Node.
 struct ListNode 
 {
-    int nilai;
+    int val;
     struct ListNode *next;
 } Node;
 
@@ -20,7 +21,7 @@ struct ListNode* partition(struct ListNode* head, int x)
     while (head != NULL)
     {
         // Jika node lebih kecil dari x, masukkan node ke list kiri
-        if (head->nilai < x)
+        if (head->val < x)
         {
             if (leftPart == NULL)
                 leftPart = leftTail = head;
@@ -46,15 +47,95 @@ struct ListNode* partition(struct ListNode* head, int x)
         head = head->next;
     }
 
-    // Kondisi terdapat node lebih besar dari x
+    // Kondisi list kanan memiliki node
     if (rightPart != NULL)
         rightTail->next = NULL;
     
-    // Kondisi terdapat node lebih kecil dari x
+    // Kondisi list kiri memiliki node
     if (leftPart != NULL)
         leftTail->next = rightPart;
     else return rightPart; // Kembalikan list kanan jika tidak ada list kiri
 
     // Mengembalikan hasil
     return leftPart;
+}
+
+
+
+
+// Fungsi untuk membuat node baru
+struct ListNode* createNode(int val) {
+    struct ListNode* newNode = (struct ListNode*)malloc(sizeof(struct ListNode));
+    newNode->val = val;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Fungsi untuk mencetak linked list
+void printList(struct ListNode* head) {
+    struct ListNode* temp = head;
+    while (temp != NULL) {
+        printf("%d ", temp->val);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+// Fungsi untuk membuat linked list dari input pengguna
+struct ListNode* createListFromInput() {
+    int n, val;
+    struct ListNode *head = NULL, *tail = NULL;
+    
+    printf("Masukkan jumlah elemen dalam linked list: ");
+    scanf("%d", &n);
+    
+    if (n <= 0) {
+        return NULL;
+    }
+    
+    printf("Masukkan elemen-elemen:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &val);
+        struct ListNode* newNode = createNode(val);
+        
+        if (head == NULL) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
+    return head;
+}
+
+// Driver code
+int main() {
+    struct ListNode* node1 = (struct ListNode*)malloc(sizeof(struct ListNode));
+    struct ListNode* node2 = (struct ListNode*)malloc(sizeof(struct ListNode));
+    struct ListNode* node3 = (struct ListNode*)malloc(sizeof(struct ListNode));
+    struct ListNode* node4 = (struct ListNode*)malloc(sizeof(struct ListNode));
+    struct ListNode* node5 = (struct ListNode*)malloc(sizeof(struct ListNode));
+    struct ListNode* node6 = (struct ListNode*)malloc(sizeof(struct ListNode));
+    struct ListNode* node7 = (struct ListNode*)malloc(sizeof(struct ListNode));
+
+    node1->val = 1;
+    node1->next = node2;
+    node2->val = 2;
+    node2->next = node3;
+    node3->val = 3;
+    node3->next = node4;
+    node4->val = 4;
+    node4->next = node5;
+    node5->val = 3;
+    node5->next = node6;
+    node6->val = 2;
+    node6->next = node7;
+    node7->val = 1;
+    node7->next = NULL;
+
+    struct ListNode* head = partition(node1, 4); 
+    printList(head);
+    return 0;
 }
