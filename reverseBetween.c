97bed1af5e21cdb8d96a91807d176721aaad460e
SuperBypassUdinnn <maingametiaplibur86@gmail.com>
2025-05-20 10:40:11 +0700

    memperbaiki test case

--- reverseBetween.c
+++ reverseBetween.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 
 struct ListNode
@@ -96,55 +97,20 @@ struct ListNode *createListFromInput()
     return head;
 }
 
-// Driver code
-int main()
+void clearTerminal()
 {
-    struct ListNode *node1 = (struct ListNode *) malloc(sizeof(struct ListNode));
-    struct ListNode *node2 = (struct ListNode *) malloc(sizeof(struct ListNode));
-    struct ListNode *node3 = (struct ListNode *) malloc(sizeof(struct ListNode));
-    struct ListNode *node4 = (struct ListNode *) malloc(sizeof(struct ListNode));
-    struct ListNode *node5 = (struct ListNode *) malloc(sizeof(struct ListNode));
-    struct ListNode *node6 = (struct ListNode *) malloc(sizeof(struct ListNode));
-    struct ListNode *node7 = (struct ListNode *) malloc(sizeof(struct ListNode));
-
-    node1->val = 1;
-    node1->next = node2;
-    node2->val = 2;
-    node2->next = node3;
-    node3->val = 3;
-    node3->next = node4;
-    node4->val = 4;
-    node4->next = node5;
-    node5->val = 5;
-    node5->next = node6;
-    node6->val = 6;
-    node6->next = node7;
-    node7->val = 7;
-    node7->next = NULL;
-
-    struct ListNode *head = removeNthFromEnd(node1, 8);
-    printList(head);
-    return 0;
-}
-
-// Fungsi untuk mencetak linked list
-void printList(struct ListNode *head)
-{
-    struct ListNode *temp = head;
-    while (temp != NULL)
-    {
-        printf("%d ", temp->val);
-        temp = temp->next;
-    }
-    printf("\n");
+    printf("\033[2J\033[H");
 }
 
 int main()
 {
+    clearTerminal();
     struct ListNode *head = createListFromInput();
     int left, right;
-    printf("Masukkan posisi 'left' dan 'right': ");
-    scanf("%d %d", &left, &right);
+    printf("Masukkan posisi 'left': ");
+    scanf("%d", &left);
+    printf("Masukkan posisi 'right': ");
+    scanf("%d", &right);
     head = reverseBetween(head, left, right);
     printf("Linked list setelah dibalik dari posisi %d hingga %d:\n", left, right);
     printList(head);
