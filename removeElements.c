f760c5729a090937030042e00b4b72e0b320c732
SuperBypassUdinnn <maingametiaplibur86@gmail.com>
2025-05-20 10:23:46 +0700

    mengganti nama file

--- removeElements.c
+++ removeElements.c
@@ -1,27 +1,33 @@
 #include <stdio.h>
 
 // Definisi struct ListNode
-struct ListNode {
+struct ListNode
+{
     int val;
-    struct ListNode* next;
+    struct ListNode *next;
 };
 
-struct ListNode* removeElements(struct ListNode* head, int val) {
+struct ListNode *removeElements(struct ListNode *head, int val)
+{
     // Membuat dummy node untuk menangani kasus saat head perlu dihapus
     struct ListNode dummy;
     dummy.next = head;
 
     // Pointer untuk traversal
-    struct ListNode* current = &dummy;
+    struct ListNode *current = &dummy;
 
     // Lakukan traversal pada linked list
-    while (current->next != NULL) {
-        if (current->next->val == val) {
+    while (current->next != NULL)
+    {
+        if (current->next->val == val)
+        {
             // Jika node berikutnya harus dihapus, lewati node itu
-            struct ListNode* temp = current->next;
+            struct ListNode *temp = current->next;
             current->next = current->next->next;
             free(temp);
-        } else {
+        }
+        else
+        {
             current = current->next;
         }
     }
